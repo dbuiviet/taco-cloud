4d9d80f9ba29a5ee56583d4a64bb4168a62bced6
dbuiviet <dbuiviet@gmail.com>
2020-02-05 00:01:17 +0700

    143 DN

---
+++ src/test/java/com/dbuiviet/spring/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,20 @@
+package com.dbuiviet.spring.tacocloud;
+
+import com.dbuiviet.spring.tacocloud.controller.HomeController;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
